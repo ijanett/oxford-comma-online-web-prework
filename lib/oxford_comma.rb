1c6147b2a1c3f1842b0e40e0def243fb564d5ec1
Irma J Mejia <ijanettmejia@outlook.com>
2019-05-10 19:24:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     array.join (" and ")
   elsif array.length >=3
-    array.join[0,-1].join(" , ") << ", and #{array[-1]}"
+    array.join[0..-2].join(" , ") << ", and #{array[-1]}"
   else
     array.join
   end
