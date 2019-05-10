fd565ce4a93770ed2e4384e8a2508e3059a5b571
Irma J Mejia <ijanettmejia@outlook.com>
2019-05-10 19:10:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,4 @@
 def oxford_comma(array)
-  array.join
-end
 
-def oxford_comma(two_element_array)
-  two_element_array.join(" and ")
-end
-
-def oxford_comma(three_element_array)
-  three_element_array.join(" , and ")
-end
+if array.length == 2
+  array.join (" and ")
