f69ed225d1985472a460fb7bacc93dd5bc5b7b1f
Irma J Mejia <ijanettmejia@outlook.com>
2019-05-10 19:23:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,9 @@
 def oxford_comma(array)
-  array.join
+  if array.length == 2
+    array.join (" and ")
+  elsif array.length >=3
+    array.join[0,-1].join(" , ") << ", and #{array[-1]}"
+  else
+    array.join
+  end
 end
-# # def oxford_comma(multiple_element_array)
-#   if array.length == 2
-#     array.join (" and ")
-#   elsif array.length >=3
-#     array.join (" , ")
-#   else
-#
-#   end
-# end
